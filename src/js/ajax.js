0b1ef42c5dfcbf657ecf3c3e862972626747ced0
Alvin Abyan <alvin.sfb@gmail.com>
2016-10-07 16:28:19 +0700

    Upload kedua

---
+++ src/js/ajax.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#login-button").click(function(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "src/json/users.json", false);
+    request.send(null);
+    var my_JSON_object = JSON.parse(request.responseText);
+
+    var username = document.getElementById("username-form").value;
+    var password = document.getElementById("password-form").value;
+     
+    for (var i = 0; i < my_JSON_object.users.length; i++) {
+      if(my_JSON_object.users[i].username == username && my_JSON_object.users[i].password == password)
+        window.location.replace("game.html");
+    }
+  });
+});
