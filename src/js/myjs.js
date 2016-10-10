9377e2921ad9e252deadc9fa4448d3e9acc98194
izzatr <m.izzatraihan@gmail.com>
2016-10-10 14:23:43 +0700

    update v1.0 izzat

---
+++ src/js/myjs.js
@@ -0,0 +1,161 @@
+$(document).ready(function(){
+
+$("#startgame").click(function(){
+	(function(){
+
+	function set(key, value) { localStorage.setItem(key, value); }
+  	function get(key)        { return localStorage.getItem(key); }
+  	function increase(el)    { set(el, parseInt( get(el) ) + 1); }
+  	function decrease(el)    { set(el, parseInt( get(el) ) - 1); }
+
+	var Memory = {
+
+		init: function(cards){
+			this.$game = $(".game");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
+
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
+
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.binding();
+			this.paused = false;
+     		this.guess = null;
+		},
+
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
+				}
+				if($(".matched").length == $(".card").length){
+					_.win();
+				}
+			}
+		},
+
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		// Fisher--Yates Algorithm -- http://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
+
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			return frag;
+		}
+	};
+
+	var cards = [
+		{
+			name: "php",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
+			id: 1,
+		},
+		{
+			name: "css3",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
+			id: 2
+		},
+		{
+			name: "html5",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+			id: 3
+		},
+		{
+			name: "jquery",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+			id: 4
+		}, 
+		{
+			name: "javascript",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+			id: 5
+		},
+		{
+			name: "node",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
+			id: 6
+		},
+		{
+			name: "photoshop",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
+			id: 7
+		},
+		{
+			name: "python",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+			id: 8
+		},
+	];
+    
+	Memory.init(cards);
+
+
+	function updateStats(){
+    $('#stats').html('<div class="padded"><h2>Figures: <span>'+
+      '<b>'+get('flip_won')+'</b><i>Won</i>'+
+      '<b>'+get('flip_lost')+'</b><i>Lost</i>'+
+      '<b>'+get('flip_abandoned')+'</b><i>Abandoned</i></span></h2>'+
+      '<ul><li><b>Best Casual:</b> <span>'+toTime( get('flip_casual') )+'</span></li>'+
+      '<li><b>Best Medium:</b> <span>'+toTime( get('flip_medium') )+'</span></li>'+
+      '<li><b>Best Hard:</b> <span>'+toTime( get('flip_hard') )+'</span></li></ul>'+
+      '<ul><li><b>Total Flips:</b> <span>'+parseInt( ( parseInt(get('flip_matched')) + parseInt(get('flip_wrong')) ) * 2)+'</span></li>'+
+      '<li><b>Matched Flips:</b> <span>'+get('flip_matched')+'</span></li>'+
+      '<li><b>Wrong Flips:</b> <span>'+get('flip_wrong')+'</span></li></ul></div>');
+  	};
+
+	})();
+});
+});
