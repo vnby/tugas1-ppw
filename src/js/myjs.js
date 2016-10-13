c050ed407bdb9490153e2607bea47d10f831ecbb
Alvin Abyan <alvin.sfb@gmail.com>
2016-10-13 11:38:21 +0700

    KELARR (engine-nya)

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -1,17 +1,59 @@
 function init() {
-if(sessionStorage.username)
-document.getElementById("uname").innerHTML = sessionStorage.username;
-else
-document.getElementById("uname").innerHTML = "-";
+	if(sessionStorage.username)
+		document.getElementById("uname").innerHTML = sessionStorage.username;
+	else
+		document.getElementById("uname").innerHTML = "unknown";
 }
 
 $(document).ready(function(){
-var h3 = document.getElementsByTagName("h3")[0],
-start = document.getElementById("startid"),
-stop = document.getElementById("stop"),
-clear = document.getElementById("clear"),
-milliseconds = 0, minutes = 0, hours = 0, runtime = false,
-t;
+	var h3 = document.getElementsByTagName("h3")[0],
+	start = document.getElementById("startid"),
+	stop = document.getElementById("stop"),
+	clear = document.getElementById("clear"),
+	usrnm = sessionStorage.username,
+	milliseconds = 0, seconds = 0, minutes = 0, runtime = false, winningTime = 0, arrayPemenang = [],
+	t, pemenang;
+
+	function inputArray() {
+	pemenang = {'username' : usrnm, 'waktu' : parseInt(winningTime), 'result' : (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + ":" + (milliseconds > 9 ? milliseconds : "0" + milliseconds)}
+	if(arrayPemenang != null) {
+		arrayPemenang.push(pemenang);
+		arrayPemenang.sort(compare);
+	}
+}
+
+function compare(x,y) {
+	if(x.waktu < y.waktu) {
+		return -1;
+	} else if (x.waktu > y.waktu) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+function generateBoard() {
+	var printTab = JSON.parse(localStorage.getItem("awal"));
+	$.each(printTab, function(i, element) {
+		if(arrayPemenang != null) {
+			$("#uname" + i + "").text(element.username);
+			$("#waktu" + i + "").text(element.result);
+		}
+	})
+}
+
+function setBoard() {
+	if(localStorage.getItem("awal") == null) {
+		localStorage.setItem("awal", JSON.stringify(arrayPemenang));
+	} else {
+		var arrObj = JSON.parse(localStorage.getItem("awal"));
+		arrObj.push({'username' : usrnm, 'waktu' : parseInt(winningTime), 'result' : (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + ":" + (milliseconds > 9 ? milliseconds : "0" + milliseconds)})
+		arrObj.sort(compare);
+		localStorage.setItem("awal", JSON.stringify(arrObj));
+	}
+}
+
+generateBoard();
 
 	$("#startgame").click(function(){
 		(function(){
@@ -22,14 +64,14 @@ t;
 				milliseconds++;
 				if (milliseconds >= 100) {
 					milliseconds = 0;
-					minutes++;
-					if (minutes >= 60) {
-						minutes = 0;
-						hours++;
+					seconds++;
+					if (seconds >= 60) {
+						seconds = 0;
+						minutes++;
 					}
 				}
 
-				h3.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (milliseconds > 9 ? milliseconds : "0" + milliseconds);
+				h3.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + ":" + (milliseconds > 9 ? milliseconds : "0" + milliseconds);
 
 				timer();
 			}
@@ -72,35 +114,43 @@ t;
 					this.$memoryCards.on("click", this.cardClicked);
 				},
 
-		cardClicked: function(){
-			var _ = Memory;
-			var $card = $(this);
-			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
-				$card.find(".inside").addClass("picked");
-				if(!_.guess){
-					_.guess = $(this).attr("data-id");
-				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
-					$(".picked").addClass("matched");
-					_.guess = null;
-				} else {
-					_.guess = null;
-					_.paused = true;
-					setTimeout(function(){
-						$(".picked").removeClass("picked");
-						Memory.paused = false;
-					}, 600);
-				}
-				if($(".matched").length == $(".card").length){
-					_.win();
-				}
-			}
-		},
+				cardClicked: function(){
+					var _ = Memory;
+					var $card = $(this);
+					if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+						$card.find(".inside").addClass("picked");
+						if(!_.guess){
+							_.guess = $(this).attr("data-id");
+						} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+							$(".picked").addClass("matched");
+							_.guess = null;
+						} else {
+							_.guess = null;
+							_.paused = true;
+							setTimeout(function(){
+								$(".picked").removeClass("picked");
+								Memory.paused = false;
+							}, 600);
+						}
+						if($(".matched").length == $(".card").length){
+							_.win();
+						}
+					}
+				},
 
-		win: function(){
-			this.paused = true;
+				win: function(){
+					this.paused = true;
 			//reset timer
 			clearTimeout(t);
-			milliseconds = 0; minutes = 0; hours = 0;
+
+			//generating boards
+			winningTime = parseInt((minutes * 60000) + (seconds * 1000) + milliseconds);
+			inputArray();
+			setBoard();
+			generateBoard();
+
+			//reset the value
+			milliseconds = 0; seconds = 0; minutes = 0;
 			runtime = false;
 		},
 
@@ -179,20 +229,6 @@ var cards = [
 ];
 
 Memory.init(cards);
-
-
-function updateStats(){
-	$('#stats').html('<div class="padded"><h2>Figures: <span>'+
-		'<b>'+get('flip_won')+'</b><i>Won</i>'+
-		'<b>'+get('flip_lost')+'</b><i>Lost</i>'+
-		'<b>'+get('flip_abandoned')+'</b><i>Abandoned</i></span></h2>'+
-		'<ul><li><b>Best Casual:</b> <span>'+toTime( get('flip_casual') )+'</span></li>'+
-		'<li><b>Best Medium:</b> <span>'+toTime( get('flip_medium') )+'</span></li>'+
-		'<li><b>Best Hard:</b> <span>'+toTime( get('flip_hard') )+'</span></li></ul>'+
-		'<ul><li><b>Total Flips:</b> <span>'+parseInt( ( parseInt(get('flip_matched')) + parseInt(get('flip_wrong')) ) * 2)+'</span></li>'+
-		'<li><b>Matched Flips:</b> <span>'+get('flip_matched')+'</span></li>'+
-		'<li><b>Wrong Flips:</b> <span>'+get('flip_wrong')+'</span></li></ul></div>');
-};
 
 })();
 });
